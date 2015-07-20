36239de5b3f879d6cf4ef02c4984f082b16f0c3f
Mauro <lavaramano@gmail.com>
2015-07-20 03:48:15 -0300

    __all__ fields for modelforms in Django 1.8

--- jot/forms.py
+++ jot/forms.py
@@ -7,8 +7,10 @@ class OneLinerForm(forms.ModelForm):
     class Meta:
         widgets = {'object_id': forms.Select()}
         model = OneLiner
+	fields = '__all__'
         
 class NoteForm(forms.ModelForm):
     class Meta:
         widgets = {'object_id': forms.Select()}
         model = Note
+	fields = '__all__'
